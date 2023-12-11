5304a1631ca7cbc428af73ff025fdbd760028450
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-11 18:40:47 +0530

    Terraform - Expense setup - create subnets, route tables, associate route tables to subnets, NGW

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -38,21 +38,41 @@ resource "aws_subnet" "db" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, { Name = "public" })
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
 
 resource "aws_route_table" "web" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, { Name = "web" })
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
 }
 
 resource "aws_route_table" "app" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, { Name = "app" })
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
 }
 
 resource "aws_route_table" "db" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, { Name = "db" })
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
 }
 
 resource "aws_route_table_association" "public" {
@@ -77,4 +97,17 @@ resource "aws_route_table_association" "db" {
   count = length(aws_subnet.db)
   subnet_id      = aws_subnet.db.*.id[count.index]
   route_table_id = aws_route_table.db.id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, { Name = "igw" })
+}
+
+resource "aws_eip" "ngw" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public.*.id[0]
 }
