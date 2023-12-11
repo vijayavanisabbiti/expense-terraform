08a025fa516b9269902fe13c8e8c92b4e1b057cd
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-11 16:54:27 +0530

    Terraform - Modules - Expense setup

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,8 +7,5 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index]
-
-  tags = {
-    Name = "Main"
-  }
+  tags = merge(var.tags, { Name = "public_subnet" })
 }
