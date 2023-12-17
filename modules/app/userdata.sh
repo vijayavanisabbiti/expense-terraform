efc0c5644d3186f4e2cf90d386ed09b5f90a75db
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-17 12:49:35 +0530

    Terraform - App creation

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 yum install ansible -y | tee -a /opt/userdata.log
+# shellcheck disable=SC2154
 ansible-pull -i localhost, -U https://github.com/vijayavanisabbiti/expense-ansible expense.yml -e role_name="${role_name}" -e env="${env}" | tee -a /opt/userdata.log
