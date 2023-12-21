a47d72e3a5be9db876ac0e1721dbe3672fa45501
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-21 22:00:22 +0530

    Terraform - App load balancer creation

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -73,10 +73,12 @@ resource "aws_lb_listener" "https" {
 
 
 resource "aws_lb_listener" "http" {
+  overwrite         = true
   count             = var.enable_https ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
+
 
   default_action {
     type = "redirect"
