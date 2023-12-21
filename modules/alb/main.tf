e535e700452a5d3fa87ab99b6fff3f8a5e53f2a7
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-21 22:05:39 +0530

    Terraform - App load balancer creation

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -87,7 +87,7 @@ resource "aws_lb_listener" "http" {
       status_code = "HTTP_301"
     }
   }
-  overwrite         = true
+  tags = merge(var.tags, { Name = "${var.env}-${var.type}.alb" })
 }
 
 resource "aws_route53_record" "main" {
