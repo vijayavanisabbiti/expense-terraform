970adab537ab862488d5ebe020639b2e28b64fbe
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-22 17:14:54 +0530

    Terraform - App load balancer creation

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -79,24 +79,6 @@ provider "aws" {
   }
 }
 
-resource "aws_lb_listener" "http" {
-  count             = var.enable_https ? 1 : 0
-  load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-
 resource "aws_route53_record" "main" {
   name    = "${var.component}-${var.env}"
   type    = "CNAME"
