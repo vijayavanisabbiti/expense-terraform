9131beb05234dbde2b176b095a39dafb48be691d
VIJAYAVANI <vijayavanimanju@gmail.com>
2024-01-09 16:23:02 +0530

    Prod environment configuration

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -83,6 +83,7 @@ resource "aws_lb_target_group" "main" {
   port     = var.app_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  deregistration_delay = 10
 
   health_check {
     enabled             = true
